b2b90950733e7ee66d64e706d56828f8945bb161
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-10-05 01:34:21 +0200

    Beautifying my code a bit

--- src/Objectorienterad_Programmering_och_Java/ExaminationWork_1/Main.java
+++ src/Objectorienterad_Programmering_och_Java/ExaminationWork_1/Main.java
@@ -2,8 +2,6 @@ package Objectorienterad_Programmering_och_Java.ExaminationWork_1;
 
 public class Main {
     public static void main(String[] arg) {
-
         new PlantCareProgram().careProgram();
-
     }
 }
