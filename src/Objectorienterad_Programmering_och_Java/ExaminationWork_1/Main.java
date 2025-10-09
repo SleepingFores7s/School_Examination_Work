be7584c474d54b4e4522cf4ef78e7c7769f7b34b
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-10-10 00:36:29 +0200

    doc: added a comment

--- src/Objectorienterad_Programmering_och_Java/ExaminationWork_1/Main.java
+++ src/Objectorienterad_Programmering_och_Java/ExaminationWork_1/Main.java
@@ -3,6 +3,7 @@ package Objectorienterad_Programmering_och_Java.ExaminationWork_1;
 public class Main {
     public static void main(String[] arg) {
 
+        //Out of main!
         new PlantCareProgram().careProgram();
 
     }
