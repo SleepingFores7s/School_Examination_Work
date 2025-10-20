4d3a7f0f285bf0a29096030ea90f6f24080656c7
SleepingFores7s <Daniel.Evensson@hotmail.com>
2025-10-20 04:59:50 +0200

    add: Test text file for testing purposes

---
+++ src/Objectorienterad_Programmering_och_Java/ExaminationWork_2/BestGymEver.java
@@ -0,0 +1,41 @@
+package Objectorienterad_Programmering_och_Java.ExaminationWork_2;
+
+import javax.swing.*;
+import java.io.File;
+
+public class BestGymEver {
+    public void MemberProgram() {
+
+        //1. ask the user for name or personnummer
+        //2. Import the file data to an object within an Array.
+        //3. Check the data for if they are a current member/ past member(use LocalDate) / not a member(not in file)
+        //4. Private Trainer file save the name/ personnummer / current date for when a person is at the gym
+
+        //Essentials
+        final String GYM_DATA_PATH = "src/Objectorienterad_Programmering_och_Java/ExaminationWork_2/Data till inlämningsuppgift 2.txt";
+        ReadWriteFiles readAndWrite = new ReadWriteFiles();
+        boolean programLoop = true;
+        String userInput;
+        //Reads the file into the array
+
+
+        //Employee loop
+        do{
+
+            userInput = JOptionPane.showInputDialog("Input a members Name or ID:number");
+            System.out.println(userInput);
+            if(userInput == null){
+                JOptionPane.showMessageDialog(null, "Shutting off the program");
+                System.exit(0);
+            }
+
+            readAndWrite.readFileData(GYM_DATA_PATH);
+
+            readAndWrite.getMembershipStatus(userInput);
+
+
+
+        }while(programLoop);
+
+    }
+}
